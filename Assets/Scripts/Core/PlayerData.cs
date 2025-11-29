64f624815f28fb51c0be903cd30f9c55a6a11867
njcom3 <yutiy.gaponov@ab-games.com>
2025-11-29 18:23:37 +0100

    remove unused stuff

--- Assets/Scripts/Core/PlayerData.cs
+++ Assets/Scripts/Core/PlayerData.cs
@@ -1,20 +1,8 @@
-﻿using Core.Interfaces.Domains;
-using Zenject;
-
-namespace Core
+﻿namespace Core
 {
     public class PlayerData
     {
-        private IPlayerResourceModule[] _resourceModules;
         private string _selectedItem;
-        
-        [Inject]
-        public void Construct(IPlayerResourceModule[] resourceModules)
-        {
-            _resourceModules = resourceModules;
-        }
-
-
         
         public void SelectItem(string itemName)
         {
